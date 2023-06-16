f68e900dd6dee68418a510743b5e73a9d2c1e3ee
Jonathan Azoff <jon@azof.fr>
2023-06-15 22:44:02 -0700

    fix extract step

--- main.py
+++ main.py
@@ -58,6 +58,7 @@ def download_pdf_and_extract_text(url: str, extra_context: str = '') -> str:
 	
 	with tempfile.NamedTemporaryFile() as temp:
 		download_pdf(url, temp.name)
+		text = extract_text(temp.name)
 		text = f"{extra_context}{text}"
 	
 	cache.set(cache_key, text)
